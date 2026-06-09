6dcca9a764a9b5ea7f0f4436953c134c02fa9a5b
Karthikeyan <karthikeyan11051998@gmail.com>
2026-06-09 17:49:31 +0530

    Added S3 upload functionality

--- extraction/extract_movies.py
+++ extraction/extract_movies.py
@@ -4,6 +4,7 @@ import pandas as pd
 from dotenv import load_dotenv
 from datetime import datetime
 import json
+import boto3
 
 load_dotenv()
 
@@ -11,17 +12,24 @@ API_KEY = os.getenv("TMDB_API_KEY")
 
 url = "https://api.themoviedb.org/3/movie/popular"
 
-params = {
+all_movies = []
+
+for page in range(1, 26):
+    params = {
     "api_key": API_KEY,
     "language": "en-US",
     "page": 1
-}
+    }
 
 response = requests.get(url, params=params)
 
 response.raise_for_status()
 
-movies = response.json()["results"]
+movies = all_movies
+
+all_movies.extend(movies)
+
+print(f"Collected page {page}")
 
 timestamp = datetime.now().strftime("%Y%m%d")
 
@@ -50,6 +58,14 @@ csv_file = f"data/movies_{timestamp}.csv"
 
 df.to_csv(csv_file, index=False)
 
-print(f"JSON saved: {json_file}")
-print(f"CSV saved: {csv_file}")
-print(df.head())
+bucket_name = "movie-tmdb-data-lake"
+
+s3 = boto3.client("s3")
+
+s3.upload_file(
+    csv_file,
+    bucket_name,
+    f"raw/movies/{csv_file.split('/')[-1]}"
+)
+
+print("File uploaded to S3 successfully")
